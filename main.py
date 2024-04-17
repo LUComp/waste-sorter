8608664607b8b2892141174c69034d61f5dcf03d
Matias Barandiaran <matias.barandiaran03@gmail.com>
2024-04-17 16:35:03 +0200

    process image modularization

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import imageio.v2 as iio
 from resnet.classifier import classify_waste
 from torchvision import transforms
 
+def process_image(image):
+      transform = transforms.Compose([
+      transforms.ToPILImage(),
+      transforms.Resize([224,224]),
+      transforms.ToTensor()
+      ])
+
+      device = torch.device("cuda")
+
+      image = transform(image).unsqueeze(0).to(device)
+
+
 model = torch.load("resnet/model/trash.pth")
 model.eval()
 
 image = iio.imread("test.jpg")
 
-transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize([224,224]),
-      transforms.ToTensor()
-])
+process_image(image)
 
-device = torch.device("cuda")
+label = classify_waste(model, image, True)
 
-image = transform(image).unsqueeze(0).to(device)
-
-print(classify_waste(model, image, True))
+print(label)
 
 
 
