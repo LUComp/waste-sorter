95b43121b1bdee8d4c73ad18d26d4d208944b6e4
Jeong Jun Ahn <tyyyu1268@gmail.com>
2024-04-18 17:12:19 +0200

    preview

--- detection/detection.py
+++ detection/detection.py
@@ -1,19 +1,7 @@
 import cv2
-import imageio.v2 as iio
 from skimage.metrics import structural_similarity as compare_ssim
-from time import sleep
-import os
 
-# Compare current frame to background
-def compare_frame_with_image(frame, reference_image):
-    img_reference = cv2.imread(reference_image)
-    frame_resized = cv2.resize(frame, (img_reference.shape[1], img_reference.shape[0]))
-    mse = (frame_resized ** 2).mean()
-    ssim = compare_ssim(frame_resized, img_reference) 
-    return mse, ssim
-
-# Compare current frame from image
-def is_frame_different_from_image(frame, reference_image, threshold_ssim=0.95):
+def is_frame_different_from_image(frame, reference_image, threshold_ssim=0.90):
     img_reference = cv2.imread(reference_image)
     frame_resized = cv2.resize(frame, (img_reference.shape[1], img_reference.shape[0]))
     gray_frame = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
@@ -25,50 +13,27 @@ def is_frame_different_from_image(frame, reference_image, threshold_ssim=0.95):
     else:
         return True  # Frame is different from the reference image
 
-def recognition_loop(cap, reference_image):
-    while True:
-        object_captured = False
-        _, frame = cap.read() 
-        if is_frame_different_from_image(frame, reference_image):
-            iio.imwrite('c2.jpg', iio.imread('c1.jpg'))
-            #sleep(0.5)
-            print("Object detected.")
 
-            while True:
-                # Take reference picture C2. Take photos every frame, to detect object movement. Replace c2 with new photo
-                _, frame = cap.read()
+def detect_movement():
+    c2 = cv2.imread("c2.jpg")
 
-                if not is_frame_different_from_image(frame,reference_image='c2.jpg'):
-                    cv2.imwrite('waste.jpg', frame)
-                    object_captured = True
-                    break
-                else:
-                    cv2.imwrite('c2.jpg', frame)
-                    sleep(0.25)
-            
-        # video preview
-        # cv2.imshow('Frame', frame)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q') or object_captured:
-            break
+    if is_frame_different_from_image(c2, "c1.jpg"):
+        print("Movement detected")
+        return True
+    
+    return False
+    
 
-def get_waste_image():
-    # Get Background Reference
-    reference_image = "c1.jpg"
+def detect_object():
+    cap = cv2.VideoCapture(4)  # Try different API here
+    ret, frame = cap.read()
+    cv2.imwrite("c3.jpg", frame)
+    print("RET", ret)
 
-    # Set camera to rolling
-    cap = cv2.VideoCapture(1) 
-    recognition_loop(cap, reference_image)
-    # Release the capture and close all OpenCV windows
-    cap.release()
-    # cv2.destroyAllWindows()
-    waste_image = iio.imread("waste.jpg")
-    print("Done")
-
-    if  os.path.exists("c1.jpg") and os.path.exists("c2.jpg") and os.path.exists("waste.jpg"):
-        # os.remove("c1.jpg")
-        os.remove("c2.jpg")
-        os.remove("waste.jpg")
-
-    return waste_image
-
+    if not is_frame_different_from_image(frame,"c2.jpg"):
+        print("Done")
+        cv2.imwrite('waste.jpg', frame)
+        return 'waste.jpg'
+    
+    print("Object moving")
+    return ""
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# import torch
-# from resnet.classifier import classify_waste
-from detection.detection2 import detect_movement, detect_object
-import cv2
+import seriallib.armcontroller
+import seriallib.exceptions
+import torch
+from resnet.classifier import classify_waste
+from detection.detection import detect_movement, detect_object
+import seriallib
 import time
+import cv2
 
-# model = torch.load("resnet/model/trash.pth")
-# model.eval()
+model = torch.load("resnet/model/trash.pth")
+model.eval()
+
+armcontroller = seriallib.ArmController("mock") # dont connect to arm over serial, just say it is successful instantly
+## ensure arduino ide/anything else using the serial port is closed.
+# armcontroller = seriallib.ArmController("/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8513332303635140E1A0-if00") # this is correct for the standard arduino on linux
+# armcontroller = seriallib.ArmController("COM3") # windows, change to match serial port shown in arduino ide?
+
+bin1_labels = ["metal", "glass"]
+bin2_labels = ["paper", "cardboard"]
+bin3_labels = ["trash", "plastic"]
 
 
 def camera_preview():
       starttime = time.time()
       frame = None
 
-      while time.time() - starttime < 0.2:
-            cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # Try different API here
+      while time.time() - starttime < 0.1:
+            cap = cv2.VideoCapture(4)  # Try different API here
             ret, frame = cap.read()
             if not ret:
                   print("Error: Failed to capture frame")
@@ -23,18 +35,18 @@ def camera_preview():
                   break
 
       cap.release()
-      # cv2.destroyAllWindows()
       cv2.imwrite('c2.jpg',frame)
 
 
 def main():
       print("started")
-      cap = cv2.VideoCapture(1) 
+      cap = cv2.VideoCapture(4) 
       ret, frame = cap.read()
       # print("RET", ret)
       print("img taken")
       cv2.imwrite('c1.jpg',frame)
       cap.release()
+      img = ""
 
       while True:
             camera_preview()
@@ -42,6 +54,27 @@ def main():
                   img = detect_object()
                   if img != "":
                         break
+                  
+      # img present, pickup with arm.
+      armcontroller.grab()
+      
+      # process the image and classify it
+      label = classify_waste(model, img, output_as_string=True)
+      
+      try:
+            if label in bin1_labels:
+                  armcontroller.move_bin1()
+            elif label in bin2_labels:
+                  armcontroller.move_bin2()
+            else:
+                  # fallback to bin 3
+                  armcontroller.move_bin3()
+      except seriallib.exceptions.ArmException as e:
+            print(e)
+      
+      print(label)
+
+      cv2.destroyAllWindows()
                         
 
 main()
