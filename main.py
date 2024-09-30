a88a6ac90da86e205f0ee0831237eb0d299e6f93
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-09-30 17:12:54 +0100

    Deleted code irrelevant for object detection and positioning

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-import seriallib.armcontroller
-import seriallib.exceptions
-import seriallib.armcontroller
-import seriallib.exceptions
-import torch
-from resnet.classifier import classify_waste
 from detection.detection import detect_movement, detect_object
-import seriallib
 import time
 import cv2
 import imageio.v2 as iio
-
-model = torch.load("resnet/model/trash.pth")
-model.eval()
-
-armcontroller = seriallib.ArmController("mock") # dont connect to arm over serial, just say it is successful instantly
-## ensure arduino ide/anything else using the serial port is closed.
-# armcontroller = seriallib.ArmController("/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8513332303635140E1A0-if00") # this is correct for the standard arduino on linux
-# armcontroller = seriallib.ArmController("COM3") # windows, change to match serial port shown in arduino ide?
-
-bin1_labels = ["metal", "glass", "misc"]
-bin2_labels = ["paper", "cardboard", "plastic"]
 
 def camera_preview():
       starttime = time.time()
@@ -56,26 +38,5 @@ def main():
                         img = detect_object()
                         if img != "":
                               break
-
-            # process the image and classify it
-            label = classify_waste(model, iio.imread(img), output_as_string=True)
-
-            print(label)
-
-            try:
-                  # img present, pickup with arm.
-                  armcontroller.grab()
-            except seriallib.exceptions.ArmException as e:
-                  print(e)
-
-            try:
-                  if label in bin1_labels:
-                        armcontroller.move_bin1()
-                  elif label in bin2_labels:
-                        armcontroller.move_bin2()
-
-            except seriallib.exceptions.ArmException as e:
-                  print(e)
                         
-
 main()
