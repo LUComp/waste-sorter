e96520b84a4c45698359fec21a2519100fd74f88
Matias Barandiaran <gitmambo@gmail.com>
2024-10-03 13:14:54 +0200

    fixed reflections

--- vision/detect.py
+++ vision/detect.py
@@ -20,31 +20,37 @@ def process_frame(frame, model):
     h_pixel = 0
     w_pixel = 0
 
-    # Loop for detected objects
-    for _, row in df.iterrows():
-        # Coordinates
-        x_min = int(row['xmin'])
-        y_min = int(row['ymin'])
-        x_max = int(row['xmax'])
-        y_max = int(row['ymax'])
+    df['area'] = (df['xmax'] - df['xmin']) * (df['ymax'] - df['ymin'])
+    largest = df.loc[df['area'].idxmax()]
 
-        # Draw rectangle
-        cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+    confidence = largest['confidence']
 
-        # Calculate the midpoint of the rectangle
-        x_pixel = (x_min + x_max) // 2
-        y_pixel = (y_min + y_max) // 2
-        h_pixel = y_max - y_min
-        w_pixel = x_max - x_min
+    if df.empty or confidence < 0.4:
+        return frame, is_detected, 0, 0, 0, 0
+    
+    # Coordinates
+    x_min = int(largest['xmin'])
+    y_min = int(largest['ymin'])
+    x_max = int(largest['xmax'])
+    y_max = int(largest['ymax'])
 
-        # Draw a red dot at the center of the rectangle
-        cv2.circle(frame, (x_pixel, y_pixel), 5, (0, 0, 255), -1)
+    # Draw rectangle
+    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-        # If the rectangle's center is close to the middle of the frame, set "True"
-        if abs(x_pixel - frame_mid_x) < 100:  # 50 pixel proximity tolerance
-            is_detected = True 
+    # Calculate the midpoint of the rectangle
+    x_pixel = (x_min + x_max) // 2
+    y_pixel = (y_min + y_max) // 2
+    h_pixel = y_max - y_min
+    w_pixel = x_max - x_min
 
-    return frame, is_detected, x_pixel, y_pixel, h_pixel, w_pixel
+    # Draw a red dot at the center of the rectangle
+    cv2.circle(frame, (x_pixel, y_pixel), 5, (0, 0, 255), -1)
+
+    # If the rectangle's center is close to the middle of the frame, set "True"
+    if abs(x_pixel - frame_mid_x) < 100:  # 50 pixel proximity tolerance
+        is_detected = True 
+
+    return frame, is_detected, x_pixel, y_pixel, w_pixel, h_pixel
      
 
 
