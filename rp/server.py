6327c4e35c782beb798c98dc3a16736cff13afcd
Matias Barandiaran <gitmambo@gmail.com>
2024-10-03 16:58:28 +0200

    dont close server on client disconnect

--- rp/server.py
+++ rp/server.py
@@ -1,8 +1,19 @@
 import bluetooth
 import RPi.GPIO as GPIO
-from rp.servo import set_angle
+from servo import set_angle
 
-if __name__ == "main":
+
+def handle_client(client_socket, client_address):
+    print(f"Accepted connection from {client_address}")
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024)  # Receive up to 1024 bytes
+        if not data:
+            break
+        set_angle(pwm, pin, float(data.decode('utf-8')))
+
+
+if __name__ == "__main__":
     # Set GPIO numbering mode
     GPIO.setmode(GPIO.BOARD)
 
@@ -16,7 +27,7 @@ if __name__ == "main":
 
     # Bind the socket to any available port on the Bluetooth adapter
     port = bluetooth.PORT_ANY
-    server_socket.bind(("14:13:33:0A:AF:32", port))
+    server_socket.bind(("", port))
 
     # Start listening for incoming connections (backlog of 1)
     server_socket.listen(1)
@@ -24,19 +35,17 @@ if __name__ == "main":
     bluetooth_address, server_port = server_socket.getsockname()
 
     print(f"Server is listening on Bluetooth address: {bluetooth_address}, port: {server_port}")
-
-    client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address}")
-
-    try:
+    try: 
         while True:
-            # Receive data from the client
-            data = client_socket.recv(1024)  # Receive up to 1024 bytes
-            if not data:
-                break
-            set_angle(pwm, pin, float(data.decode('utf-8')))
-
+            print("Ready to accept connection...")
+            client_socket, client_address = server_socket.accept()
+            try:
+                handle_client(client_socket, client_address)        
+            finally:
+                # Close the sockets
+                client_socket.close()
     finally:
-        # Close the sockets
-        client_socket.close()
         server_socket.close()
+
+
+
